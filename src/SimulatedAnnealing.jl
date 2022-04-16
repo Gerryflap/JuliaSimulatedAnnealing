d842078501612a2654abcc81b9f32f2102f36705
gerryflap <gerry2211@gmail.com>
2022-04-16 13:37:29 +0200

    first commit

---
+++ src/SimulatedAnnealing.jl
@@ -0,0 +1,76 @@
+module SimulatedAnnealing
+    export State, SimState, Settings, annealing_step, run_simulated_annealing
+
+    # Models the current state
+    abstract type State end
+
+    mutable struct SimState
+        s::State
+        step::Int64
+        e_current::Float64
+    end
+
+    struct Result
+        s::State
+        e::Float64
+        energies::Union{Array{Float64}, Nothing}
+    end
+
+    struct Settings
+        # Function that gets the faction of the time used so far and returns a temperature. Should be 0 at the end
+        temperature_fn
+        # Function that computes the energy of the given State. Lower is better
+        energy_fn
+        # Function that generates a random neighbour state given the current state
+        neighbour_fn
+        # Max steps taken by the algorithm
+        max_steps::Int64
+    end
+
+    # Computes the acceptance probability of the new state given the current and new energy values and the temperature
+    function p(e_current, e_new, temperature) :: Float64
+        if e_new < e_current
+            return 1
+        else
+            return exp(-(e_new - e_current)/temperature)
+        end
+    end
+
+    # Performs one step of the simulated annealing algorithm (in-place on the SimState)
+    function annealing_step(simstate::SimState, settings::Settings)
+        s_new = settings.neighbour_fn(simstate.s)
+        simstate.step += 1
+        temperature = settings.temperature_fn(simstate.step / settings.max_steps)
+        e_current = simstate.e_current
+        e_new = settings.energy_fn(s_new)
+        acceptance_probability = p(e_current, e_new, temperature)
+        if rand() <= acceptance_probability
+            simstate.s = s_new
+            simstate.e_current = e_new
+        end
+    end
+
+    # Runs simulated annealing from s_initial with the given settings
+    function run_simulated_annealing(s_initial::State, settings::Settings; collect_energy_vals::Bool=false) :: Result
+        simstate = SimState(s_initial, 0, settings.energy_fn(s_initial))
+
+        energy_vals = nothing
+        if collect_energy_vals
+            # Collect results
+            energy_vals = zeros(Float64, settings.max_steps + 1)
+            energy_vals[1] = simstate.e_current
+
+            while simstate.step < settings.max_steps
+                annealing_step(simstate, settings)
+                energy_vals[simstate.step + 1] = simstate.e_current
+            end
+        else 
+            # Just execute without collecting
+            while simstate.step < settings.max_steps
+                annealing_step(simstate, settings)
+            end
+        end
+
+        return Result(simstate.s, simstate.e_current, energy_vals)
+    end
+end # module
---
+++ src/SortingExample.jl
@@ -0,0 +1,113 @@
+module SortingExample
+    include("SimulatedAnnealing.jl")
+    using Statistics
+    using Plots
+    gr()
+
+    struct SortState <: SimulatedAnnealing.State
+        arr :: Array{Float64}
+    end
+
+    # Function that gets the faction of the time used so far and returns a temperature. Should be 0 at the end
+    function temperature_fn(budget_used::Float64) :: Float64
+        return 1-budget_used
+    end
+
+    # Computes energy based on number of element pairs that is in incorrect order
+    function energy_fn(sortState::SortState) :: Float64
+        wrong :: Float64 = 0
+        for i in 2:length(sortState.arr)
+            if sortState.arr[i-1] > sortState.arr[i]
+                wrong += 1
+            end
+        end
+        return wrong/length(sortState.arr)
+    end
+
+    # Function that computes the energy of the given State based on 
+    # the sum of how much larger the max of all elements on the left and how much smaller the min of all elements on the right is
+    # This is done over a normalized array
+    function minmax_based_energy_fn(sortState::SortState) :: Float64
+        normalized = normalize(sortState.arr)
+
+        energy = 0
+
+        max_left = normalized[1]
+        for i in 2:length(normalized)
+            diff = (max_left - normalized[i])
+            if diff > 0
+                energy += diff
+            else
+                max_left = normalized[i]
+            end            
+        end
+
+        max_right = normalized[length(normalized)]
+        for i in (length(normalized) - 1):-1:1
+            diff = (normalized[i] - max_right)
+            if diff > 0
+                energy += diff
+            else
+                max_right = normalized[i]
+            end      
+        end
+
+        return energy
+    end
+
+    # Normalizes the given array to 0 mean and 1 standard deviation
+    function normalize(arr::Array{Float64}) :: Array{Float64}
+        return (arr.-mean(arr))./std(arr)
+    end
+
+
+    # Function that generates a random neighbour state given the current state by randomly swapping 2 neighbouring elements
+    function neighbour_fn(sortState::SortState)
+        copied = copy(sortState.arr)
+        # Random number between 1 and length - 1
+        index = 1 + abs(rand(Int32) % (length(copied) - 1))
+
+        # Swap elements
+        temp = copied[index]
+        copied[index] = copied[index+1]
+        copied[index+1] = temp
+
+        return SortState(copied)
+    end
+
+    # Function that generates a random neighbour state given the current state by randomly swapping 2 random elements
+    function global_neighbour_fn(sortState::SortState)
+        copied = copy(sortState.arr)
+        # Random number between 1 and length - 1
+        index_1 = 1 + abs(rand(Int32) % (length(copied)))
+        index_2 = 1 + abs(rand(Int32) % (length(copied)))
+
+        # Swap elements
+        temp = copied[index_1]
+        copied[index_1] = copied[index_2]
+        copied[index_2] = temp
+
+        return SortState(copied)
+    end
+
+    settings = SimulatedAnnealing.Settings(
+        temperature_fn,
+        minmax_based_energy_fn,
+        global_neighbour_fn,
+        10000
+    )
+
+    initial = SortState([5 7 1 2 0 -1 8 -10 5 2 7 9 2 5 1 2 7 10 4 1])
+    result = SimulatedAnnealing.run_simulated_annealing(initial, settings; collect_energy_vals=true)
+
+    println(result.s)
+    println("Energy: ", result.e)
+
+    display(plot(
+        result.energies,
+        title="Energy of current state per step",
+        xlabel="step",
+        ylabel="energy",
+        legend=false
+    ))
+end
